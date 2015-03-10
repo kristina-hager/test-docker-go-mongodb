653ae878e4877f501eef5c3040294d5f75cc62bf
Kristina Hager <kristina.hager@gmail.com>
2015-03-10 11:08:24 -0500

    from test-docker-golang

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"runtime"
+)
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello there you beautiful world:  I was built on ", runtime.GOOS, " with a CPU ", runtime.GOARCH)
+}
+
+func main() {
+	http.HandleFunc("/", indexHandler)
+	http.ListenAndServe(":8080", nil)
+}
