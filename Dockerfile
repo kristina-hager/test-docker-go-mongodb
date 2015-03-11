59faccb668ba3b2883438cbb763c65f6d297e3b8
Kristina Hager <kristina.hager@gmail.com>
2015-03-11 12:15:01 -0500

    first commit talking to mongodb

--- Dockerfile
+++ Dockerfile
@@ -1,2 +1,16 @@
-FROM golang:onbuild
+##Dockerizing a go web app
+FROM golang
+
+MAINTAINER Kristina Hager <kristina.hager@gmail.com>
+
+#don't include git history in this image
+RUN echo ".git" > .dockerignore
+
+#mgo for talking to mongodb
+RUN go get gopkg.in/mgo.v2
+
+COPY . /go/src/test-docker-go-mongodb
+RUN go install test-docker-go-mongodb
+
+ENTRYPOINT /go/bin/test-docker-go-mongodb
 EXPOSE 8080
--- main.go
+++ main.go
@@ -2,15 +2,90 @@ package main
 
 import (
 	"fmt"
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+	"log"
 	"net/http"
+	"os"
 	"runtime"
 )
 
+type Person struct {
+	Name  string
+	Phone string
+}
+
 func indexHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Hello there you beautiful world:  I was built on ", runtime.GOOS, " with a CPU ", runtime.GOARCH)
+	fmt.Fprint(w, "Hello there you beautiful world:  I was built on ", runtime.GOOS, " with a CPU ", runtime.GOARCH, "\n")
+
+	log.Print("db uri: ", os.Getenv("DB_PORT_27017_TCP_ADDR"))
+	//log.Print("env vars: ", os.Environ())
+
+	//KH: seems strange to me to rely on DB_PORT (name the commandline gave to linked container)
+	//instructions online call for relying on MONGOHQ_URL
+	//is there some way to programmatically set MONGOHQ_URL to the same val as appropriate env var from docker CLO?
+	uri := os.Getenv("DB_PORT_27017_TCP_ADDR")
+	if uri == "" {
+		fmt.Fprint(w, "no db connection port provided")
+		log.Print("read-no db connect port provided")
+		return
+	}
+
+	sess, err := mgo.Dial(uri)
+	if err != nil {
+		fmt.Fprint(w, "Can't connect to mongo, go error: ", err)
+		log.Fatal("read-Can't connect to mongo, go error: ", err)
+		return
+	}
+	defer sess.Close()
+
+	sess.SetSafe(&mgo.Safe{})
+
+	c := sess.DB("test").C("people")
+
+	result := Person{}
+	err = c.Find(bson.M{"name": "Ale"}).One(&result)
+	if err != nil {
+		fmt.Fprint(w, "Fatal error: ", err, "\n")
+		log.Print("find fail: ", err)
+		return
+	}
+
+	fmt.Fprint(w, "retrieved phone number: ", result.Phone, "\n")
+}
+
+func loadDatabase() {
+	log.Print("---start write to database---")
+	//KH: seems strange to me to rely on DB_PORT (name the commandline gave to linked container)
+	//instructions online call for relying on MONGOHQ_URL
+	//is there some way to programmatically set MONGOHQ_URL to the same val as appropriate env var from docker CLO?
+	uri := os.Getenv("DB_PORT_27017_TCP_ADDR")
+	if uri == "" {
+		log.Fatal("no db connect port provided")
+		return
+	}
+
+	sess, err := mgo.Dial(uri)
+	if err != nil {
+		log.Fatal("write-Can't connect to mongo, go error", err)
+	}
+	defer sess.Close()
+
+	sess.SetSafe(&mgo.Safe{})
+
+	//KH hard-coded names.. fix
+	c := sess.DB("test").C("people")
+	err = c.Insert(&Person{"Ale", "+55 53 8116 9639"},
+		&Person{"Bob", "+55 53 1234 1234"})
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Print("---wrote to database---")
+
 }
 
 func main() {
+	loadDatabase()
 	http.HandleFunc("/", indexHandler)
 	http.ListenAndServe(":8080", nil)
 }
